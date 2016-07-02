52e2e9570d64b5f68476a04441d5a8218273258f
Mark Redeman <markredeman@gmail.com>
2016-07-02 15:13:03 +0200

    Refactor hammington distance

--- elisp/hamming/hamming.el
+++ elisp/hamming/hamming.el
@@ -8,18 +8,23 @@
 
 (defun hamming-distance (first second)
   "Computes the hamming distance between two dna sequences"
-  (let ((first-length (length first)))
-    (if (not (= first-length (length second)))
-        (error "Both strings should be of equal length")
-      )
-    (if (= first-length 0) 0
-      (+
-       (hamming-distance (substring first 1) (substring second 1))
-       (if (= (elt first 0) (elt second 0)) 0 1)
-       )
-      )
-    )
-  )
+  (assert-equal-length first second)
+  (if (= (length first) 0)
+      0
+    (+
+     (hamming-distance-of-first-character first second)
+     (hamming-distance (substring first 1) (substring second 1)))))
+
+(defun hamming-distance-of-first-character (first second)
+  "Checks if the first character of two strings are equal
+Return 1 if they are equal and 0 otherwise"
+  (if (= (elt first 0) (elt second 0)) 0 1))
+
+(defun assert-equal-length (first second)
+  "Throws an error if both given strings aren't of equal length"
+  (if
+      (not (= (length first) (length second)))
+      (error "Both strings should be of equal length")))
 
 (provide 'hamming)
 ;;; hamming.el ends here
