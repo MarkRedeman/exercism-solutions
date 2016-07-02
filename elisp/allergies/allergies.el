c14827bea5388ef58dcd6b5c8c76f97a8c4403ce
Mark Redeman <markredeman@gmail.com>
2016-07-02 20:07:31 +0200

    Reuse allergen-list in allergic-to-p

--- elisp/allergies/allergies.el
+++ elisp/allergies/allergies.el
@@ -2,25 +2,12 @@
 
 ;;; Commentary:
 
-;; The list of items (and their value) that were tested are:
-(defvar allergies
-  '((1 . "eggs")
-    (2 . "peanuts")
-    (4 . "shellfish")
-    (8 . "strawberries")
-    (16 . "tomatoes")
-    (32 . "chocolate")
-    (64 . "pollen")
-    (128 . "cats")))
-
 ;;; Code:
 (defun allergen-list (value)
-  (allergen-list-internal value 1)
-  )
+  (allergen-list-internal value 1))
 
 (defun allergic-to-p (value allergy)
-  (let ((allergy-value (value-for-allergy allergy)))
-    (is-allergy-of value allergy-value)))
+  (member allergy (allergen-list value)))
 
 ;; I don't quite like it that I'm using an "internal" function
 (defun allergen-list-internal (value power)
@@ -28,17 +15,23 @@
   (if (not (> power 128))
       (let ((list (allergen-list-internal value (* power 2))))
          (if (is-allergy-of value power)
-             (cons (allergen-for-value power) list)
-           list))))
+             (cons (allergen-for-value power) list) list))))
 
 (defun is-allergy-of (value allergy-value)
   (= (logand value allergy-value) allergy-value))
 
+;; The list of items (and their value) that were tested are:
 (defun allergen-for-value (value)
-    (cdr (assoc value allergies)))
-
-(defun value-for-allergy (allergen)
-    (car (rassoc allergen allergies)))
+  (let ((allergies
+         '((1 . "eggs")
+           (2 . "peanuts")
+           (4 . "shellfish")
+           (8 . "strawberries")
+           (16 . "tomatoes")
+           (32 . "chocolate")
+           (64 . "pollen")
+           (128 . "cats"))))
+    (cdr (assoc value allergies))))
 
 (provide 'allergies)
 
