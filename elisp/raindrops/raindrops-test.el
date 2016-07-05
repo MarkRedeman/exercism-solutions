11e1c7a32780c1441c96c14beec271b67e390675
Mark Redeman <markredeman@gmail.com>
2016-07-05 23:17:43 +0200

    Simplify raindrops exercise
    Doing the prime factorization was not necessary

--- elisp/raindrops/raindrops-test.el
+++ elisp/raindrops/raindrops-test.el
@@ -6,38 +6,6 @@
 
 (load-file "raindrops.el")
 
-(ert-deftest 2-is-a-prime ()
-  (should (equal '(2)
-                 (prime-factors-of 2))))
-
-(ert-deftest 3-is-a-prime ()
-  (should (equal '(3)
-                 (prime-factors-of 3))))
-
-(ert-deftest 4-is-2-times-2 ()
-  (should (equal '(2 2)
-                 (prime-factors-of 4))))
-
-(ert-deftest 5-is-a-prime ()
-  (should (equal '(5)
-                 (prime-factors-of 5))))
-
-(ert-deftest 6-is-2-times-3 ()
-  (should (equal '(2 3)
-                 (prime-factors-of 6))))
-
-(ert-deftest 8-is-2-times-2-times-2 ()
-  (should (equal '(2 2 2)
-                 (prime-factors-of 8))))
-
-(ert-deftest 9-is-3-times-3 ()
-  (should (equal '(3 3)
-                 (prime-factors-of 9))))
-
-(ert-deftest 1755-is-3-times-3-times-3-times-3-times-5-times-13 ()
-  (should (equal '(3 3 3 5 13)
-                 (prime-factors-of 1755))))
-
 (ert-deftest test-1 ()
   (should (equal "1"
                  (convert 1))))
