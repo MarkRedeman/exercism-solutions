87907281d3a3c27bde3445632dfdbf8ba7703fe0
Mark Redeman <markredeman@gmail.com>
2024-04-24 19:56:48 +0200

    Update reverse string implementation

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,11 +1,5 @@
-use unicode_reverse::reverse_grapheme_clusters_in_place;
+use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    // Original solution without grapheme clusters
-    // input.chars().rev().collect()
-
-    // Solution using unicode_reverse
-    let mut input = input.to_string();
-    reverse_grapheme_clusters_in_place(&mut input);
-    input
+    input.graphemes(true).rev().collect()
 }
